4c1a091e04de57f964bfaae48a044e3250663fff
trikitrok <trikitrok@gmail.com>
2016-09-09 09:13:33 +0200

    started refactoring to polymorphism using protocols

--- src/parrot_refactoring/core.clj
+++ src/parrot_refactoring/core.clj
@@ -7,9 +7,17 @@
 (defn- compute-base-speed-for-voltage [voltage]
   (min 24.0 (* voltage base-speed)))
 
+(defprotocol Parrot
+  (speed [this]))
+
+(defrecord EuropeanParrot [type]
+  Parrot
+  (speed [_]
+    base-speed))
+
 (defn speed [parrot]
   (case (:type parrot)
-    :european-parrot base-speed
+    :european-parrot (speed parrot)
     :african-parrot (max 0.0 (- base-speed (* load-factor (:num-coconuts parrot))))
     :norwegian-blue-parrot (if (:nailed parrot)
                              0.0
@@ -17,7 +25,9 @@
     (throw "Should be unreachable!")))
 
 (defn make [type num-coconuts voltage nailed]
-  {:type type
-   :num-coconuts num-coconuts
-   :voltage voltage
-   :nailed nailed})
+  (if (= type :european-parrot)
+    (->EuropeanParrot :european-parrot)
+    {:type type
+     :num-coconuts num-coconuts
+     :voltage voltage
+     :nailed nailed}))
